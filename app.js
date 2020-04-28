f9397edbbfac589ae9afbde610e6c033cb1d98e1
Annytitinha <anaiana.comex@gmail.com>
2020-04-28 11:27:41 +0000

    fixing app js

--- app.js
+++ app.js
@@ -3,70 +3,88 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 //const request = require("request");
-const http = require('http');
+const path = require('path');
 
 const app = express();
 
+// Bodyparser Middleware
+//app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+//app.get("/", function(req, res){
+    //res.sendFile(__dirname + "/signup.html");
+//});
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
-});
+// Signup Route
+app.post('/signup', (req, res) => {
+  const { firstName, lastName, email } = req.body;
 
-app.post ("/", function(req, res){
+  // Make sure fields are filled
+  if (!firstName || !lastName || !email) {
+    res.redirect('/fail.html');
+    return;
+  }
 
-    var firtName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+//app.post ("/", function(req, res){
+
+  //  var firtName = req.body.fName;
+    //var lastName = req.body.lName;
+    //var email = req.body.email;
+
     // data object js
-    var data = {
-      members:[
-        {
-          email_address: email,
-          status:"subscribed",
-          merge_fields:{
-            FNAME: firtName,
-            LNAME: lastName
-          }
+     // Construct req data
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
         }
-      ]
-    };
-    //tunring it into flagpack json, passing data into a string Json format
-    //const jsonData = JSON.stringfy(data);
-    // 
-    const url = "https://us8.api.mailchimp.com/3.0/lists/fd5e2741f8";
-    //creating options
-    const options = {
-      method:"POST",
-      auth:"anaiana:ead193d671e84a5aaea6eb44ec76dc0d-us8"
-    }
-    // cnheck the date that we get send back
-  const request = https.request(url, options,function(response){
-    if (response.statusCode == 200){
-      res.sendFile(__dirname + "/success.html");
-    }else {
-      res.sendFile(__dirname + "/failure.html");
-    }
+      }
+    ]
+  };
+  //tunring it into flagpack json, passing data into a string Json format
+  const postData = JSON.stringify(data);
 
-    response.on("data", function(data){
-      console.log(JSON.parse(data));
-    });
+  const options = {
+    url: 'us8.api.mailchimp.com/3.0/lists/fd5e2741f8',
+    method: 'POST',
+    headers: {
+      Authorization: 'auth anaiana:ead193d671e84a5aaea6eb44ec76dc0d-us8'
+    },
+    body: postData
+  };
+
+    
+   
+    // cnheck the date that we get send back
+  request(options, (err, response, body) => {
+    if (err) {
+      res.redirect('/fail.html');
+    } else {
+      if (response.statusCode === 200) {
+        res.redirect('/success.html');
+      } else {
+        res.redirect('/fail.html');
+      }
+    }
   });
+});
 // passing the data to the mailchimp server
  // request.write(jsonData);
-  request.end();
+ // request.end();
   //}
 
-});
+//});
 
-app.post ("/failure", function(req, res){
-  res.redirect ("/")
-})
+//app.post ("/failure", function(req, res){
+  //res.redirect ("/")
+//})
 
 app.listen(process.env.PORT|| 3000, function() {
      console.log("Server is running on port 3000");
